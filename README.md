f7c55425ab3ee0bb5cb729512385941e2612b062
sxcem <sxcem@163.com>
2017-07-20 14:52:04 +0800

    添加文件

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # cn.edu.jfcs
 Eclipse3.2 Rcp 编程学习源代码履历
+
+github tags 的创建
+
+1 文件夹右键Tortoisegit--> 创建 tag 输入版本号 选择branch下拉列表中 需要将版本号放置的主控分支位置
+2 文件夹右键Tortoisegit --> push --> 
+   Remote中选择分支项 Options中勾选Include Tags  -->   ok 即可把当前版本上传至 上面建立的Tags版本
+   
+下载所需版本源代码 打开下述网址
+https://github.com/sxcem/cn.edu.jfcs/tree/Development
+选择Branch 中Development 选中Tags版本 点击右侧 Clone or download 按钮
+Download Zip 即可
---
+++ src/cn/edu/jfcs/sys/IAppConstants.java
@@ -0,0 +1,23 @@
+/*
+*
+*经费测算系统
+*
+*Writer: xuchun.sang
+*Date：2017-7-19
+*/
+package cn.edu.jfcs.sys;
+
+//常量文本接口类
+
+public interface IAppConstants {
+	public static final String APPLICATION_TITLE="TOC精算系统";
+	public static final String APPLICATION_ID = "cn.edu.jfcs";
+	public static final String PIE_DIAGRAM_VIEW_ID = "cn.edu.jfcs.ui.piediagram";
+	public static final String TEACH_UNIT_CLASS_VIEW_ID = "cn.edu.jfcs.ui.teachunitclass";
+	public static final String TEACH_UNIT_NAME_VIEW_ID = "cn.edu.jfcs.ui.teachunitname";
+	public static final String TEACH_UNIT_DATA_VIEW_ID = "cn.edu.jfcs.ui.teachunitdata";
+	public static final String PUBLIC_DATA_EDITOR_ID = "cn.edu.jfcs.ui.publicdataeditor";
+	public static final String DEFAULT_PERSPECTIVE = "cn.edu.jfcs.perspective";
+	public static final String STACKED_BAR_CHART_VIEW_ID = "cn.edu.jfcs.ui.stackedbarchart";
+	public static final String HISTORY_DATA_VIEW_ID = "cn.edu.jfcs.ui.historydata";
+}
